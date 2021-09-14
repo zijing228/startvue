2bf6797db0524857a00b43cf518b4b6289eee41b
zijing228 <zijing228@126.com>
2021-09-14 21:18:08 +0800

    iview

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
+import iView from 'iview'; 
 import App from './App.vue'
 import router from './router'
-
+import 'iview/dist/styles/iview.css'; 
 Vue.config.productionTip = false
-
+Vue.use(iView);   
 new Vue({
   router,
   render: h => h(App)
